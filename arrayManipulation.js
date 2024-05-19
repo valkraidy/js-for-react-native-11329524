1f95ad82d1a2c88032988d7a1fea6b1e49b422b7
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2024-05-18 14:52:21 -1200

    Done with task one

---
+++ arrayManipulation.js
@@ -0,0 +1,13 @@
+function processArray(numbers){
+let i =0;
+while(i < numbers.length){
+  if(numbers[i] % 2 === 0){
+    numbers[i]=Math.pow(numbers[i],2)
+  } else {
+    numbers[i] = Math.pow(numbers[i],3)
+  }
+ i++;
+}
+return numbers;
+}
+console.log(processArray([1,2,3,4,5]));
